eaa8fc9034186734bfef218089b586ea01e1e9bc
Thembinkosi <thembinkosidladla318@gmail.com>
2025-04-22 11:11:35 +0200

    Add validation: Check length and numeric characters

--- app/src/main/java/validate_sa_id/ValidateSaId.java
+++ app/src/main/java/validate_sa_id/ValidateSaId.java
@@ -2,7 +2,17 @@ package validate_sa_id;
 
 public class ValidateSaId {
     public static boolean isIdNumberValid(String idNumber) {
-       // return false; // Stub for now
+         // Check if the input is null or not exactly 13 digits
+         if (idNumber == null || idNumber.length() != 13) {
+            return false;
+        }
+
+        // Check if the input contains only digits
+        if (!idNumber.matches("\\d+")) {
+            return false;
+        }
+
+        // return false; 
        return idNumber.equals("2001014800086") || idNumber.equals("2909035800085");
  }
 }
