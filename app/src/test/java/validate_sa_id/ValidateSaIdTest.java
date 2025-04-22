18a592eddcb6b7fda99f608c754decde9ed00f71
Thembinkosi <thembinkosidladla318@gmail.com>
2025-04-22 12:06:43 +0200

    Add tests for too short Id

--- app/src/test/java/validate_sa_id/ValidateSaIdTest.java
+++ app/src/test/java/validate_sa_id/ValidateSaIdTest.java
@@ -1,5 +1,6 @@
 package validate_sa_id;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,5 +11,12 @@ public class ValidateSaIdTest {
         assertTrue(ValidateSaId.isIdNumberValid("2001014800086"));
         assertTrue(ValidateSaId.isIdNumberValid("2909035800085"));
     }
+     //test for 12 digits
+    @Test
+    public void testIsIdNumberValidTooShort() {
+        assertFalse(ValidateSaId.isIdNumberValid("123456789012")); // 12 digits
+    }
+
+
 
 }
